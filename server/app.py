f54fb5006ebeca183f61caefb4becad1e5c31ff3
Elijah M <elijah.macharia@student.moringaschool.com>
2025-06-20 13:00:58 +0300

    Complete Flask routing and views lab

--- server/app.py
+++ server/app.py
@@ -2,45 +2,35 @@ from flask import Flask
 
 app = Flask(__name__)
 
-# Index route: returns an h1 title as required by test
+
 @app.route('/')
 def index():
     return '<h1>Python Operations with Flask Routing and Views</h1>'
 
-# Print string route: returns plain text, not HTML
+
 @app.route('/print/<string:param>')
 def print_string(param):
-    print(param)  # Logs to console
+    print(param)
     return param
 
-# Count route: returns numbers on separate lines with a trailing newline
+
 @app.route('/count/<int:param>')
 def count(param):
-    numbers = '\n'.join(str(i) for i in range(param)) + '\n'
-    return numbers
+    return '\n'.join(str(i) for i in range(param))
 
-# Math route: supports +, -, *, div, %
 @app.route('/math/<int:num1>/<operation>/<int:num2>')
 def math(num1, operation, num2):
-    if operation == '+':
+    if operation == 'add':
         result = num1 + num2
-    elif operation == '-':
+    elif operation == 'sub':
         result = num1 - num2
-    elif operation == '*':
+    elif operation == 'mult':
         result = num1 * num2
     elif operation == 'div':
-        if num2 == 0:
-            return 'Error: Division by zero is not allowed.'
         result = num1 / num2
-    elif operation == '%':
-        if num2 == 0:
-            return 'Error: Modulo by zero is not allowed.'
+    elif operation == 'mod':
         result = num1 % num2
     else:
-        return 'Invalid operation.'
+        return 'Invalid operation'
 
     return str(result)
-
-# Run the Flask app
-if __name__ == '__main__':
-    app.run(debug=True)
